419a436ee84e26be76b373e67885c3fc73aff861
Akshayjilwane2003 <97149467+Akshayjilwane2003@users.noreply.github.com>
2024-04-25 13:31:48 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,7 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        
+        stack = []
+
+        for char in s:
+            if char in '({[':
+                stack.append(char)
+            elif char == ')' and (not stack or stack.pop() != '('):
+                return False
+            elif char == '}' and (not stack or stack.pop() != '{'):
+                return False
+            elif char == ']' and (not stack or stack.pop() != '['):
+                return False
+
+        return not stack
         pass
     
 

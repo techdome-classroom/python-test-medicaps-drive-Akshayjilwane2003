8ed3727532d4c6c2691cfd118c3b8a20292ddab5
Akshayjilwane2003 <97149467+Akshayjilwane2003@users.noreply.github.com>
2024-04-25 14:02:47 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,27 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        roman_values = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = roman_values[s[-1]]  # Initialize total with the value of the last character
         
-        result = 0
-        prev_value = 0
-        
-        for i in range(len(s) - 1, -1, -1):
-            current_value = roman_values[s[i]]
-            
-            if current_value >= prev_value:
-                result += current_value
+        for i in range(len(s) - 2, -1, -1):  # Iterate from second to the last character to the first character
+            if roman_values[s[i]] < roman_values[s[i + 1]]:
+                total -= roman_values[s[i]]
             else:
-                result -= current_value
-            
-            prev_value = current_value
+                total += roman_values[s[i]]
         
-        return result
+        return total
